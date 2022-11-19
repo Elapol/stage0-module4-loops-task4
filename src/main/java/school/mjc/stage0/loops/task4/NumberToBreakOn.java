832c8724a45b0f91b3243a3cd4d2c8fe314f371c
elyad <ella.dikovich@gmail.com>
2022-11-19 15:08:23 +0100

    ela

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,7 +1,16 @@
 package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
-    public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+    public static void main(String[] args){
+        printNumbersUntilWithBreakOn(8,7);
+    }
+    public static void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        for (int counter=0;counter<=numberToGoUntil;counter++){
+            if(counter==toBreakWith){
+                break;
+            } if(numberToGoUntil==counter){ System.out.println("iterating till the end ");}
+            System.out.println(counter);
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -1,7 +1,16 @@
 package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
-    public void printUntilButThird(int lastPrinted) {
+    public static void main(String[] args){
+        printUntilButThird(10);
+    }
+    public static void printUntilButThird(int lastPrinted) {
+        for(int counter=1;counter<=lastPrinted;counter++){
+            if(counter%3==0){
+                continue;
+
+            } System.out.println(counter);
+        }
 
     }
 }
